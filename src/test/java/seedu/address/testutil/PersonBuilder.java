2ecde74de7f8f2db9388e45ef222ee0296ba8d6b
taniatsq <e0969220@u.nus.edu>
2024-03-05 02:47:22 +0800

    Update PersonBuilder

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -99,7 +99,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags, remark);
+        return new Person(name, phone, email, address, remark, tags);
     }
 
 }

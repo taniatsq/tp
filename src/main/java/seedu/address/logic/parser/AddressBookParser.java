2430071af9dc7ac531667d905da1e836c5c8c411
AndrewOng2066 <e0959899@u.nus.edu>
2024-03-25 17:53:32 +0800

    Add new function deleteAttendanceRecordCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -13,6 +13,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CreateClassCommand;
+import seedu.address.logic.commands.DeleteAttendanceRecordCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditAttendanceCommand;
 import seedu.address.logic.commands.EditCommand;
@@ -93,6 +94,9 @@ public class AddressBookParser {
 
         case EditAttendanceCommand.COMMAND_WORD:
             return new EditAttendanceCommandParser().parse(arguments);
+
+        case DeleteAttendanceRecordCommand.COMMAND_WORD:
+            return new DeleteAttendanceRecordCommandParser().parse(arguments);
 
         case SelectClassCommand.COMMAND_WORD:
             return new SelectClassCommandParser().parse(arguments);
